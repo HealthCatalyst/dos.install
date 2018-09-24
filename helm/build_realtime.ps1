4840608999afa2c1f9903fac9b49039e93e91883
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-09-24 14:48:32 -0700

    rename to fabricrealtime
    Signed-off-by: Imran Qureshi <imran.qureshi@healthcatalyst.com>

--- helm/build_realtime.ps1
+++ helm/build_realtime.ps1
@@ -4,26 +4,28 @@
 # specification for charts: https://docs.helm.sh/developing_charts
 
 Write-Host "Running lint"
-helm lint dos-realtime
+helm lint fabricrealtime
 
 Write-Host "Remove existing tar"
-Remove-Item -Path "dos-realtime-*.tgz"
+Remove-Item -Path "fabricrealtime-*.tgz"
 
 Write-Host "Packaging realtime"
-helm package dos-realtime
+helm package fabricrealtime
 
-Write-Host "moving dos-realtime-*.tgz tp releases/"
-Move-Item dos-realtime-*.tgz releases/ -Force
+Write-Host "moving fabricrealtime-*.tgz tp releases/"
+Move-Item fabricrealtime-*.tgz releases/ -Force
 
 Write-Host "Updating index"
 helm repo index releases/ --url https://raw.githubusercontent.com/HealthCatalyst/dos.install/master/helm/releases/
 
 Write-Host "Remove existing tar"
-Remove-Item -Path "dos-realtime-*.tgz" -Force
+Remove-Item -Path "fabricrealtime-*.tgz" -Force
 
 Write-Host "--------------------------------------------------"
 Write-Host "After you commit this change, the chart will be ready to serve"
 Write-Host "You can add it to a local helm by: helm repo add dos https://raw.githubusercontent.com/HealthCatalyst/dos.install/master/helm/releases"
+Write-Host "If you already have it, run this to update the list: helm repo update"
 
-Write-Host "And install realtime via: helm install -f myvalues.yaml dos/dos-realtime --name=realtime"
+Write-Host "And install realtime via: helm install -f myvalues.yaml dos/fabricrealtime --name=fabricrealtime --namespace fabricrealtime --verify --wait"
+Write-Host "To uninstall a release: helm del --purge realtime"
 Write-Host "--------------------------------------------------"
