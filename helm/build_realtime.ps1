4a8ed4cde4e39ab5a82bfaf064d241994cfac55a
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-09-24 14:26:09 -0700

    rename dos.realtime to dos-realtime since helm doesn't like .
    Signed-off-by: Imran Qureshi <imran.qureshi@healthcatalyst.com>

--- helm/build_realtime.ps1
+++ helm/build_realtime.ps1
@@ -3,24 +3,27 @@
 
 # specification for charts: https://docs.helm.sh/developing_charts
 
+Write-Host "Running lint"
+helm lint dos-realtime
+
 Write-Host "Remove existing tar"
-Remove-Item -Path "dos.realtime-*.tgz"
+Remove-Item -Path "dos-realtime-*.tgz"
 
 Write-Host "Packaging realtime"
-helm package dos.realtime
+helm package dos-realtime
 
-Write-Host "moving realtime-*.tgz tp releases/"
-Move-Item dos.realtime-*.tgz releases/ -Force
+Write-Host "moving dos-realtime-*.tgz tp releases/"
+Move-Item dos-realtime-*.tgz releases/ -Force
 
 Write-Host "Updating index"
 helm repo index releases/ --url https://raw.githubusercontent.com/HealthCatalyst/dos.install/master/helm/releases/
 
 Write-Host "Remove existing tar"
-Remove-Item -Path "dos.realtime-*.tgz" -Force
+Remove-Item -Path "dos-realtime-*.tgz" -Force
 
 Write-Host "--------------------------------------------------"
 Write-Host "After you commit this change, the chart will be ready to serve"
-Write-Host "You can add it to a local helm by: helm repo add realtime https://raw.githubusercontent.com/HealthCatalyst/dos.install/master/helm/releases"
+Write-Host "You can add it to a local helm by: helm repo add dos https://raw.githubusercontent.com/HealthCatalyst/dos.install/master/helm/releases"
 
-Write-Host "And install realtime via: helm install releases/realtime --name=realtime"
+Write-Host "And install realtime via: helm install -f myvalues.yaml dos/dos-realtime --name=realtime"
 Write-Host "--------------------------------------------------"
