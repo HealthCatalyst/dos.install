8a8693511e791529f271c575209887a173feb1af
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-09-24 14:08:06 -0700

    rename realtime to dos.realtime
    Signed-off-by: Imran Qureshi <imran.qureshi@healthcatalyst.com>

--- helm/build_realtime.ps1
+++ helm/build_realtime.ps1
@@ -1,19 +1,22 @@
 # builds the release
+# based on: https://medium.com/containerum/how-to-make-and-share-your-own-helm-package-50ae40f6c221
+
+# specification for charts: https://docs.helm.sh/developing_charts
 
 Write-Host "Remove existing tar"
-Remove-Item -Path "realtime-*.tgz"
+Remove-Item -Path "dos.realtime-*.tgz"
 
 Write-Host "Packaging realtime"
-helm package realtime
+helm package dos.realtime
 
 Write-Host "moving realtime-*.tgz tp releases/"
-Move-Item realtime-*.tgz releases/ -Force
+Move-Item dos.realtime-*.tgz releases/ -Force
 
 Write-Host "Updating index"
 helm repo index releases/ --url https://raw.githubusercontent.com/HealthCatalyst/dos.install/master/helm/releases/
 
 Write-Host "Remove existing tar"
-Remove-Item -Path "realtime-*.tgz" -Force
+Remove-Item -Path "dos.realtime-*.tgz" -Force
 
 Write-Host "--------------------------------------------------"
 Write-Host "After you commit this change, the chart will be ready to serve"
