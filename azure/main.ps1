762dfe18f7137e65c7bf6507f7259fd43bf3972e
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-10-12 12:22:06 -0700

    check version of AzureRM
    Signed-off-by: Imran Qureshi <imran.qureshi@healthcatalyst.com>

--- azure/main.ps1
+++ azure/main.ps1
@@ -1,5 +1,5 @@
 param([bool]$prerelease, [bool]$local)
-$version = "2018.10.12.06"
+$version = "2018.10.12.07"
 [Console]::ResetColor()
 Write-Host "--- main.ps1 version $version ---"
 Write-Host "prerelease flag: $prerelease"
@@ -39,8 +39,29 @@ Write-Host "GITHUB_URL: $GITHUB_URL"
 
 Write-Host "Powershell version: $($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor).$($PSVersionTable.PSVersion.Build)"
 
+$module = "AzureRM"
+$minVersion = "6.10.0"
+if (Get-Module -ListAvailable -Name $module) {
+    Write-Host "Module $module exists"
+
+    Import-Module -Name $module
+    $moduleInfo = $(Get-Module -Name "$module")
+    if ($null -eq $moduleInfo) {
+        Install-Module -Name $module -MinimumVersion $minVersion -AllowClobber
+    }
+    else {
+        Write-Host "Checking Version of $module module is $minVersion"
+        if ($minVersion -ne $moduleInfo.Version.ToString()) {
+            Install-Module -Name $module -MinimumVersion $minVersion -AllowClobber
+        }
+    }
+}
+else {
+    Write-Host "Module $module does not exist"
+    Install-Module -Name $module -MinimumVersion $minVersion -AllowClobber
+}
+
 Import-Module PowerShellGet -Force
-Import-Module AzureRM -Force
 
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $topLevelFolder = "$here\..\..\"
