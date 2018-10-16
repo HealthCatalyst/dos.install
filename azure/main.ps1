f225601df88a5441b954b1c98deb49ede3943938
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-10-16 08:48:58 -0700

    add support for pulling in a specific branch
    Signed-off-by: Imran Qureshi <imran.qureshi@healthcatalyst.com>

--- azure/main.ps1
+++ azure/main.ps1
@@ -1,14 +1,14 @@
-param([bool]$prerelease, [bool]$local)    
+param([string]$branch, [bool]$local)
 $version = "2018.06.06.01"
 Write-Host "--- main.ps1 version $version ---"
-Write-Host "prerelease flag: $prerelease"
+Write-Host "branch flag: $branch"
 
 # http://www.rlmueller.net/PSGotchas.htm
 # Trap {"Error: $_"; Break;}
 # Set-StrictMode -Version latest
 
 if ($local) {
-    Write-Host "use local files: $local"    
+    Write-Host "use local files: $local"
 }
 
 # https://stackoverflow.com/questions/9948517/how-to-stop-a-powershell-script-on-the-first-error
@@ -23,13 +23,13 @@ $InformationPreference = "Continue"
 #   curl -useb https://raw.githubusercontent.com/HealthCatalyst/dos.install/master/azure/main.ps1 | iex;
 #   curl -sSL  https://raw.githubusercontent.com/HealthCatalyst/dos.install/master/azure/main.ps1 | pwsh -Interactive -NoExit -c -;
 
-if ($prerelease) {
+if ($branch) {
     if($local){
-        #$GITHUB_URL = "."
-        $GITHUB_URL = "https://raw.githubusercontent.com/HealthCatalyst/dos.install/master"
+        $GITHUB_URL = "."
+        #$GITHUB_URL = "https://raw.githubusercontent.com/HealthCatalyst/dos.install/$branch"
     }
     else {
-        $GITHUB_URL = "https://raw.githubusercontent.com/HealthCatalyst/dos.install/master"
+        $GITHUB_URL = "https://raw.githubusercontent.com/HealthCatalyst/dos.install/$branch"
     }
 }
 else {
@@ -46,35 +46,35 @@ if ($local) {
     Get-Content ./common/common-kube.ps1 -Raw | Invoke-Expression;
 }
 else {
-    Invoke-WebRequest -useb ${GITHUB_URL}/common/common-kube.ps1?f=$randomstring | Invoke-Expression;    
+    Invoke-WebRequest -useb ${GITHUB_URL}/common/common-kube.ps1?f=$randomstring | Invoke-Expression;
 }
 
 if ($local) {
     Get-Content ./common/common.ps1 -Raw | Invoke-Expression;
 }
 else {
-    Invoke-WebRequest -useb ${GITHUB_URL}/common/common.ps1?f=$randomstring | Invoke-Expression;    
+    Invoke-WebRequest -useb ${GITHUB_URL}/common/common.ps1?f=$randomstring | Invoke-Expression;
 }
 
 if ($local) {
     Get-Content ./common/common-azure.ps1 -Raw | Invoke-Expression;
 }
 else {
-    Invoke-WebRequest -useb ${GITHUB_URL}/common/common-azure.ps1?f=$randomstring | Invoke-Expression;    
+    Invoke-WebRequest -useb ${GITHUB_URL}/common/common-azure.ps1?f=$randomstring | Invoke-Expression;
 }
 
 if ($local) {
     Get-Content ./common/product-menu.ps1 -Raw | Invoke-Expression;
 }
 else {
-    Invoke-WebRequest -useb ${GITHUB_URL}/common/product-menu.ps1?f=$randomstring | Invoke-Expression;    
+    Invoke-WebRequest -useb ${GITHUB_URL}/common/product-menu.ps1?f=$randomstring | Invoke-Expression;
 }
 
 if ($local) {
     Get-Content ./common/troubleshooting-menu.ps1 -Raw | Invoke-Expression;
 }
 else {
-    Invoke-WebRequest -useb ${GITHUB_URL}/common/troubleshooting-menu.ps1?f=$randomstring | Invoke-Expression;    
+    Invoke-WebRequest -useb ${GITHUB_URL}/common/troubleshooting-menu.ps1?f=$randomstring | Invoke-Expression;
 }
 
 # if(!(Test-Path .\Fabric-Install-Utilities.psm1)){
@@ -89,12 +89,12 @@ while ($userinput -ne "q") {
     if (Test-CommandExists kubectl) {
         $currentcluster = $(kubectl config current-context 2> $null)
     }
-    
+
     Write-Host "================ Health Catalyst version $version, common: $(GetCommonVersion) azure: $(GetCommonAzureVersion) kube: $(GetCommonKubeVersion) ================"
-    if ($prerelease) {
-        Write-Host "prerelease flag: $prerelease"
+    if ($branch) {
+        Write-Host "branch flag: $branch"
     }
-    Write-Warning "CURRENT CLUSTER: $currentcluster"    
+    Write-Warning "CURRENT CLUSTER: $currentcluster"
     Write-Host "0: Change kube to point to another cluster"
     Write-Host "------ Infrastructure -------"
     Write-Host "1: Create a new Azure Container Service"
@@ -140,53 +140,53 @@ while ($userinput -ne "q") {
             $folders = Get-ChildItem "C:\kubernetes" -directory
             for ($i = 1; $i -le $folders.count; $i++) {
                 Write-Host "$i. $($folders[$i-1])"
-            }              
+            }
             $index = Read-Host "Enter number of folder to use (1 - $($folders.count))"
             $folderToUse = $($folders[$index - 1])
 
             SwitchToKubCluster -folderToUse "C:\kubernetes\$folderToUse"
-        } 
+        }
         '1' {
             $config = $(ReadConfigFile).Config
             Write-Host $config
-        
+
             CreateACSCluster -baseUrl $GITHUB_URL -config $config
             ConfigureKubernetes -config $config
             SetupAzureLoadBalancer -baseUrl $GITHUB_URL -config $config -local $local
             WriteDNSCommands
-        } 
+        }
         '2' {
             $config = $(ReadConfigFile).Config
             Write-Host $config
-        
+
             ConfigureKubernetes -config $config
             SetupAzureLoadBalancer -baseUrl $GITHUB_URL -config $config -local $local
             WriteDNSCommands
-        } 
+        }
         '3' {
-            Do { 
+            Do {
                 $AKS_PERS_RESOURCE_GROUP = Read-Host "Resource Group"
             }
             while ([string]::IsNullOrWhiteSpace($AKS_PERS_RESOURCE_GROUP))
 
-            StartVMsInResourceGroup -resourceGroup $AKS_PERS_RESOURCE_GROUP 
-        } 
+            StartVMsInResourceGroup -resourceGroup $AKS_PERS_RESOURCE_GROUP
+        }
         '4' {
-            Do { 
+            Do {
                 $AKS_PERS_RESOURCE_GROUP = Read-Host "Resource Group"
             }
             while ([string]::IsNullOrWhiteSpace($AKS_PERS_RESOURCE_GROUP))
-            StopVMsInResourceGroup -resourceGroup $AKS_PERS_RESOURCE_GROUP 
-        } 
+            StopVMsInResourceGroup -resourceGroup $AKS_PERS_RESOURCE_GROUP
+        }
         '5' {
             $expiresOn = $(az account get-access-token --query "expiresOn" -o tsv)
             Do { $confirmation = Read-Host "Your current access token expires on $expiresOn. Do you want to login again to get a new access token? (y/n)"}
             while ([string]::IsNullOrWhiteSpace($confirmation))
-        
+
             if ($confirmation -eq "y") {
                 RenewAzureToken
             }
-        }         
+        }
         '6' {
             $DNS_RESOURCE_GROUP = Read-Host "Resource group containing DNS zones? (default: dns)"
             if ([string]::IsNullOrWhiteSpace($DNS_RESOURCE_GROUP)) {
@@ -197,9 +197,9 @@ while ($userinput -ne "q") {
             $customerid = $customerid.ToLower().Trim()
 
             $dnsrecordname = "$customerid.healthcatalyst.net"
-                    
+
             ShowNameServerEntries -dnsResourceGroup $DNS_RESOURCE_GROUP -dnsrecordname $dnsrecordname
-        } 
+        }
         '7' {
             $DNS_RESOURCE_GROUP = Read-Host "Resource group containing DNS zones? (default: dns)"
             if ([string]::IsNullOrWhiteSpace($DNS_RESOURCE_GROUP)) {
@@ -214,45 +214,45 @@ while ($userinput -ne "q") {
             $loadBalancerIPResult = GetLoadBalancerIPs
             $EXTERNAL_IP = $loadBalancerIPResult.ExternalIP
 
-            SetupDNS -dnsResourceGroup $DNS_RESOURCE_GROUP -dnsrecordname $dnsrecordname -externalIP $EXTERNAL_IP 
+            SetupDNS -dnsResourceGroup $DNS_RESOURCE_GROUP -dnsrecordname $dnsrecordname -externalIP $EXTERNAL_IP
         }
         '8' {
             WriteDNSCommands
-        } 
+        }
         '9' {
             Write-Host "Current cluster: $(kubectl config current-context)"
             kubectl version --short
             kubectl get "nodes"
-        } 
+        }
         '10' {
             Write-Host "If you didn't setup DNS, add the following entries in your c:\windows\system32\drivers\etc\hosts file to access the urls from your browser"
             $loadBalancerIPResult = GetLoadBalancerIPs
             $EXTERNAL_IP = $loadBalancerIPResult.ExternalIP
 
             $dnshostname = $(ReadSecretValue -secretname "dnshostname" -namespace "default")
-            Write-Host "$EXTERNAL_IP $dnshostname"            
-        }         
+            Write-Host "$EXTERNAL_IP $dnshostname"
+        }
         '20' {
             Write-Host "Current cluster: $(kubectl config current-context)"
             kubectl version --short
             kubectl get "deployments,pods,services,ingress,secrets,nodes" --namespace=kube-system -o wide
-        } 
+        }
         '21' {
             LaunchAzureKubernetesDashboard
-        } 
-        '22' {        
+        }
+        '22' {
             ShowSSHCommandsToVMs
-        } 
+        }
         '23' {
             RestartDNSPodsIfNeeded
-        } 
+        }
         '24' {
             RestartAzureVMsInResourceGroup
-        } 
+        }
         '25' {
             Read-Host "Script needs elevated privileges to flushdns.  Hit ENTER to launch script to set PATH"
             Start-Process powershell -verb RunAs -ArgumentList "ipconfig /flushdns"
-        } 
+        }
         '26' {
             $DEFAULT_RESOURCE_GROUP = ReadSecretData -secretname azure-secret -valueName resourcegroup
             CopyKubernetesSecretsToKeyVault -resourceGroup $DEFAULT_RESOURCE_GROUP
@@ -263,12 +263,12 @@ while ($userinput -ne "q") {
         }
         '30' {
             TestAzureLoadBalancer
-        } 
+        }
         '31' {
             $DEFAULT_RESOURCE_GROUP = ReadSecretData -secretname azure-secret -valueName resourcegroup
-            
+
             if ([string]::IsNullOrWhiteSpace($AKS_PERS_RESOURCE_GROUP)) {
-                Do { 
+                Do {
                     $AKS_PERS_RESOURCE_GROUP = Read-Host "Resource Group: (default: $DEFAULT_RESOURCE_GROUP)"
                     if ([string]::IsNullOrWhiteSpace($AKS_PERS_RESOURCE_GROUP)) {
                         $AKS_PERS_RESOURCE_GROUP = $DEFAULT_RESOURCE_GROUP
@@ -277,37 +277,37 @@ while ($userinput -ne "q") {
                 while ([string]::IsNullOrWhiteSpace($AKS_PERS_RESOURCE_GROUP))
             }
             FixLoadBalancers -resourceGroup $AKS_PERS_RESOURCE_GROUP
-        } 
+        }
         '32' {
             $config = $(ReadConfigFile).Config
             Write-Host $config
-        
+
             SetupAzureLoadBalancer -baseUrl $GITHUB_URL -config $config -local $local
             WriteDNSCommands
-        }         
+        }
         '33' {
             OpenTraefikDashboard
-        } 
+        }
         '50' {
             showTroubleshootingMenu -baseUrl $GITHUB_URL -isAzure $true
             $skip = $true
-        }                 
+        }
         '51' {
             showMenu -baseUrl $GITHUB_URL -namespace "fabricnlp" -isAzure $true
             $skip = $true
-        } 
+        }
         '52' {
             showMenu -baseUrl $GITHUB_URL -namespace "fabricrealtime" -isAzure $true
             $skip = $true
-        } 
+        }
         '53' {
             showMenu -baseUrl $GITHUB_URL -namespace "fabricmachinelearning" -isAzure $true
             $skip = $true
-        } 
+        }
         '54' {
             showMenu -baseUrl $GITHUB_URL -namespace "fabricehr" -isAzure $true
             $skip = $true
-        } 
+        }
         'q' {
             return
         }
@@ -316,7 +316,7 @@ while ($userinput -ne "q") {
         $userinput = Read-Host -Prompt "Press Enter to continue or q to exit"
         if ($userinput -eq "q") {
             return
-        }    
+        }
     }
     [Console]::ResetColor()
     Clear-Host
