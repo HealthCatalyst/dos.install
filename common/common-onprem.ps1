bd45110cfd5a8458be9746c7f53ea50d5bd85d48
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-10-24 11:23:52 -0400

    update docker versions
    Signed-off-by: Imran Qureshi <imran.qureshi@healthcatalyst.com>

--- common/common-onprem.ps1
+++ common/common-onprem.ps1
@@ -5,8 +5,8 @@ function global:GetCommonOnPremVersion() {
     return $versiononpremcommon
 }
 
-$dockerversion = "18.06.1.ce-3"
-$dockerselinuxversion = "17.03.3.ce-1"
+$dockerversion = "18.03.1.ce-1."
+$dockerselinuxversion = "17.03.2.ce-1"
 
 # 18.06.1.ce-3
 # The list of validated docker versions was updated to 1.11.1, 1.12.1, 1.13.1, 17.03, 17.06, 17.09, 18.06. (#68495)
