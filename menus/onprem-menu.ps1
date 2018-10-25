f50ad615f370187aad50f4484e8d68cadcfe4e50
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-10-25 08:33:56 -0700

    increment version
    Signed-off-by: Imran Qureshi <imran.qureshi@healthcatalyst.com>

--- menus/onprem-menu.ps1
+++ menus/onprem-menu.ps1
@@ -1,5 +1,5 @@
 param([ValidateNotNullOrEmpty()][string]$baseUrl, [string]$prerelease)
-$version = "2018.10.25.03"
+$version = "2018.10.25.04"
 Write-Host "--- onprem-menu.ps1 version $version ---"
 Write-Host "baseUrl = $baseUrl"
 Write-Host "prerelease flag: $prerelease"
@@ -92,7 +92,7 @@ function InstallOrUpdateModule() {
 
 InstallOrUpdateModule -module "DosInstallUtilities.Kube" -local $local -minVersion "1.72"
 
-InstallOrUpdateModule -module "DosInstallUtilities.OnPrem" -local $local -minVersion "1.62"
+InstallOrUpdateModule -module "DosInstallUtilities.OnPrem" -local $local -minVersion "1.63"
 
 # show Information messages
 $InformationPreference = "Continue"
